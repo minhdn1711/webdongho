67d3dd8cc776de3c3a279a99f777a48b737b021f
minhdn1711 <minh2tl2k@gmail.com>
2026-05-20 14:06:42 +0700

    update code

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -66,7 +66,7 @@ const menuItems = [
     { name: 'Webhook Logs', icon: 'M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z', url: '/admin/pancake/webhook-logs' },
 ];
 
-const openMenus = ref({ 'Sản phẩm': true });
+const openMenus = ref({ 'Sản phẩm': false });
 const toggleMenu = (name) => { openMenus.value[name] = !openMenus.value[name]; };
 const isChildActive = (item) => item.children?.some(c => page.url.startsWith(c.url));
 </script>
